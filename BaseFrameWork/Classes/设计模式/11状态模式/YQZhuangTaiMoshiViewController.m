686da3b9e23c9602c1ef1833c8214ad058fb35b8
晚秋 <wanqiu@cecelive.com>
2019-01-15 10:03:09 +0800

    删除测试代码 增加状态模式使用说明

--- BaseFrameWork/Classes/设计模式/11状态模式/YQZhuangTaiMoshiViewController.m
+++ BaseFrameWork/Classes/设计模式/11状态模式/YQZhuangTaiMoshiViewController.m
@@ -18,6 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /** 如果公共的代理方法中有需要用到的参数回调，可以使用在不中状态下定义各自的block  通过block将控制器用到的数据传递给manager 再通过manager定义的代理传递到控制器 即所有交互都通过manager */
     
     YQZhuangTaiManager *manager = [[YQZhuangTaiManager alloc]init];
     manager.type = ZhuangTaiType_first;
